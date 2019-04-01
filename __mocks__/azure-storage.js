0aed24bdb3296faf0cae8994762540a47fa12d9c
mamolin <mamolin@microsoft.com>
2019-04-01 14:11:39 -0700

    mocks

--- __mocks__/azure-storage.js
+++ __mocks__/azure-storage.js
@@ -12,5 +12,7 @@ module.exports = {
     createBlobService: function () {
         return mocks;
     },
-    ExponentialRetryPolicyFilter: jest.fn().mockImplementation(() => {return jest.fn()})
+    ExponentialRetryPolicyFilter: jest.fn().mockImplementation(() => {return jest.fn()}),
+    LinearRetryPolicyFilter: jest.fn().mockImplementation(() => {return jest.fn()}),
+    RetryPolicyFilter: jest.fn().mockImplementation(() => {return jest.fn()})
 };
